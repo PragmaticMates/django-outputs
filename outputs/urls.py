b55b402f8985f4cb202b65528e342b7e2e5c2322
rsoltys <radovan.soltys@gmail.com>
2020-05-12 14:36:05 +0200

    clean up

--- outputs/urls.py
+++ outputs/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path(pgettext_lazy("url", 'schedulers/<int:pk>/'), SchedulerDetailView.as_view(), name='scheduler_detail'),
     path(pgettext_lazy("url", 'schedulers/<int:pk>/update/'), SchedulerUpdateView.as_view(), name='scheduler_update'),
     path(pgettext_lazy("url", 'schedulers/<int:pk>/delete/'), SchedulerDeleteView.as_view(), name='scheduler_delete'),
-    # path(pgettext_lazy("url", 'schedulers/<int:pk>/changelog/'), ChangelogView.as_view(view_class=SchedulerDetailView), name='scheduler_changelog'),
     path(pgettext_lazy("url", 'schedulers/create/from-export/<int:export_pk>/'), SchedulerCreateView.as_view(), name='scheduler_create_from_export'),
     path(pgettext_lazy("url", 'schedulers/create/'), SchedulerCreateView.as_view(), name='scheduler_create'),
     path(pgettext_lazy('url', 'schedulers/'), SchedulerListView.as_view(), name='scheduler_list'),
