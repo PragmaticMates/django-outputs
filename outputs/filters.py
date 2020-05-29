3509a1de43d4ce1ed13548ece8f5d476eb05c524
rsoltys <radovan.soltys@gmail.com>
2020-05-29 10:34:36 +0200

    export filter - add status

--- outputs/filters.py
+++ outputs/filters.py
@@ -23,7 +23,7 @@ class ExportFilter(django_filters.FilterSet):
     class Meta:
         model = Export
         fields = [
-            'id', 'created', 'format', 'context', 'content_type',
+            'id', 'created', 'format', 'context', 'content_type', 'status'
             'total', 'creator'
         ]
 
