236eaec62470badffc1e93faf92241f67cfa65dd
Erik Telepovský <erik.telepovsky@gmail.com>
2020-10-12 18:46:03 +0200

    not scheduling jobs when app is ready (let the pragmatic.rqscheduler do its job)

--- outputs/apps.py
+++ outputs/apps.py
@@ -1,20 +1,12 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
-from django.db.utils import ProgrammingError
 
 
 class OutputsConfig(AppConfig):
     name = 'outputs'
     verbose_name = _('Outputs')
 
-    def ready(self):
-        try:
-            self.schedule_jobs()
-        except ProgrammingError:
-            pass
-
     def schedule_jobs(self):
-        print('Scheduling outputs jobs...')
         from outputs.models import Scheduler
 
         for output_scheduler in Scheduler.objects.active():
