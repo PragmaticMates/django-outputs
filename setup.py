3131669ea390984bf9cf288dea5dd13f274ee839
eriktelepovsky <erik.telepovsky@gmail.com>
2021-06-22 11:03:54 +0200

    v1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-outputs',
-    version='1.0.3',
+    version='1.0.4',
     description='Exports and schedulers for Django',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
