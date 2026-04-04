8df82f5cae782a07e3d535194787e6ffbdeb0563
michalsoltys <michael.soltys8@gmail.com>
2026-04-04 18:05:20 +0200

    fix test

--- outputs/tests/settings.py
+++ outputs/tests/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'outputs',
 ]
 
+# pragmatic>=4.1 defaults to django.tasks.task (Django 5.2+). Tests use Django 4.x + RQ.
+PRAGMATIC_TASK_DECORATOR = 'django_rq.job'
+
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
